961c12029547fb533a9251eaa8c863da1d699ad3
vianavitor-dev <vianavitor232@gmail.com>
2025-06-09 12:22:09 -0300

    Classe Foco - Dono de Pet (entidade associativa) criada

---
+++ comunicavet_api/src/main/java/com/projetointegrador/comunicavet/model/PetOwnerFocus.java
@@ -0,0 +1,43 @@
+package com.projetointegrador.comunicavet.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "looking_for") // "looking for a focus"
+public class PetOwnerFocus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "focus_id")
+    private Focus focus;
+
+    @OneToMany
+    @JoinColumn(name = "pet_owner_id")
+    private PetOwner petOwner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Focus getFocus() {
+        return focus;
+    }
+
+    public void setFocus(Focus focus) {
+        this.focus = focus;
+    }
+
+    public PetOwner getPetOwner() {
+        return petOwner;
+    }
+
+    public void setPetOwner(PetOwner petOwner) {
+        this.petOwner = petOwner;
+    }
+}
