20db13f41972917ce14154ad3f4cfdf9cd7f131e
vianavitor-dev <vianavitor232@gmail.com>
2025-06-09 12:06:51 -0300

    Classe Foco criada

---
+++ comunicavet_api/src/main/java/com/projetointegrador/comunicavet/model/Focus.java
@@ -0,0 +1,41 @@
+package com.projetointegrador.comunicavet.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "focus")
+public class Focus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "short_description")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
