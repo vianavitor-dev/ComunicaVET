266d7da3724b46a454e59de5ef87a780ccc1a0a9
vianavitor-dev <vianavitor232@gmail.com>
2025-10-05 11:30:04 -0300

    Modificando StateRepository.findByName(...) para retornar apenas 1 resultado

--- comunicavet_api/src/main/java/com/projetointegrador/comunicavet/repository/StateRepository.java
+++ comunicavet_api/src/main/java/com/projetointegrador/comunicavet/repository/StateRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StateRepository extends CrudRepository<State, Long> {
-    List<State> findByName(String name);
+    Optional<State> findByName(String name);
 
     List<State> findByAbbreviation(String abbreviation);
 }
