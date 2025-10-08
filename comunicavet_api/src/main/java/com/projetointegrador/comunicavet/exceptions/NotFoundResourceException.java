e135c495ef50fb0c1a83a3c02448a2874c3d5f88
vianavitor-dev <vianavitor232@gmail.com>
2025-10-07 22:59:17 -0300

    Modificando herença da exceção NotFoundResourceException para RuntimeException

--- comunicavet_api/src/main/java/com/projetointegrador/comunicavet/exceptions/NotFoundResourceException.java
+++ comunicavet_api/src/main/java/com/projetointegrador/comunicavet/exceptions/NotFoundResourceException.java
@@ -4,7 +4,7 @@ package com.projetointegrador.comunicavet.exceptions;
 /**
 Essa exceção será lançada quando um determinado recurso no banco de dados não existir.
  **/
-public class NotFoundResourceException extends Exception {
+public class NotFoundResourceException extends RuntimeException {
     public NotFoundResourceException(String message) {
         super(message);
     }
